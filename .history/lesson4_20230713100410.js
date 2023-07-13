4713ca163e270b3d728a27b75c558aa8a3756a5f
Eve <eve.jensch@gmail.com>
2023-07-13 10:05:18 +0100

    lesson 4 project

---
+++ .history/lesson4_20230713100410.js
@@ -0,0 +1,17 @@
+//Project: Dog Years
+//variable with my age
+let myAge = 37;
+//changable variable
+let earlyYears = 2;
+earlyYears *= 10.5;
+//myAge has 2 years subtracted as they're already accounted for
+let laterYears = myAge - 2;
+//calculates the number of dog years
+laterYears *= 4;
+console.log(earlyYears, laterYears);
+let myAgeInDogYears = earlyYears + laterYears;
+//myName is returned in all lower case letters
+const myName = "Eve Jensch".toLowerCase();
+//logging ages to the console
+console.log(`My name is ${myName}. I am ${myAge} years old in human years old which is ${myAgeInDogYears} years old in dog years.`);
+//Output:
