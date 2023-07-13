a9fd999208d37d35b6a5dcf013b8d91c31d5bd2d
Eve <eve.jensch@gmail.com>
2023-07-13 10:40:54 +0100

    lesson 5

---
+++ .history/lesson5_20230713103310.js
@@ -0,0 +1,6 @@
+//If Statements
+
+if (true) {
+    console.log('This message will print!');
+}
+  // Prints: This message will print!
---
+++ .history/lesson5_20230713103755.js
@@ -0,0 +1,11 @@
+//If Statements
+
+if (true) {
+    console.log('This message will print!');
+}
+// Prints: This message will print!
+
+let sale = false;
+if (sale === true) {
+    console.log('Time to buy!');
+}
